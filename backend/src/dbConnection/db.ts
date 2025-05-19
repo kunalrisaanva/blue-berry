f80a82ae3cd94a610464fc39e941f59335e1d3de
kunalrisaanva <kunalrisaanva12@gmail.com>
2025-05-19 13:35:52 +0530

    chore: update db connection file to set default DNS result order

--- backend/src/dbConnection/db.ts
+++ backend/src/dbConnection/db.ts
@@ -1,6 +1,9 @@
 import { Pool } from "pg";
 import dotenv from "dotenv";
 dotenv.config({ path: ".env" });
+import dns from "dns";
+
+dns.setDefaultResultOrder("ipv4first");
 
 const connectionString = process.env.POSTGRESQL_URI;
 console.log("test",connectionString);
