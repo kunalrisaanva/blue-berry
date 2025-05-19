311b472045f3df91068bacf79d0873f05dc95b61
kunalrisaanva <kunalrisaanva12@gmail.com>
2025-05-19 17:05:28 +0530

    fix: remove duplicate dns import and set default result order

--- backend/src/dbConnection/db.ts
+++ backend/src/dbConnection/db.ts
@@ -1,9 +1,9 @@
+import dns from "dns";
+dns.setDefaultResultOrder("ipv4first");
 import { Pool } from "pg";
 import dotenv from "dotenv";
 dotenv.config({ path: ".env" });
-import dns from "dns";
 
-dns.setDefaultResultOrder("ipv4first");
 
 const connectionString = process.env.POSTGRESQL_URI;
 console.log("test",connectionString);
